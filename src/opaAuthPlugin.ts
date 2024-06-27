ee50822b91b4083136e10ba85751c3230499080e
Edoardo Viviani <e.viviani@treedom.net>
2024-06-27 16:02:29 +0200

    feat: add directive options

--- src/opaAuthPlugin.ts
+++ src/opaAuthPlugin.ts
@@ -4,6 +4,7 @@ import fp from 'fastify-plugin'
 import { OPAClient } from '@styra/opa'
 import mercuriusAuth, { MercuriusAuthOptions } from 'mercurius-auth'
 import mercurius from 'mercurius'
+import { parseDirectiveArgumentsAST } from './parseDirectiveArgumentsAST'
 
 export const opaAuthPlugin: FastifyPluginCallback<PluginProps> = fp(
   async (app: FastifyInstance, props: PluginProps) => {
@@ -18,16 +19,18 @@ export const opaAuthPlugin: FastifyPluginCallback<PluginProps> = fp(
       /**
        * Validate directive
        */
-      async applyPolicy(ast, parent, args, context) {
-        const path = ast.arguments.find(
-          (argument) => argument?.name?.value === 'path'
-        )?.value?.value as string
+      async applyPolicy(ast, parent, args, context, info) {
+        const { path, options } = parseDirectiveArgumentsAST(ast.arguments) as {
+          path: string
+          options?: object
+        }
 
         const allowed = await opa
           .evaluate(path, {
             headers: context.reply.request.headers,
             parent,
             args,
+            options,
           })
           .catch((err) => {
             app.log.error({ err }, 'Error while evaluating OPA policy')
